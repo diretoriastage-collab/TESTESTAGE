4b10facc539591c8803240c2aaf7953247fb3bda
diretoriastage-collab <diretoria.stage@gmail.com>
2026-06-17 21:36:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function dataParaBR(d) {
 }
 
 // ===== CONFIGURAÇÕES =====
-const GOOGLE_SHEET_VENDAS_URL = 'https://script.google.com/macros/s/AKfycbwegEqf-1McwTqsOvi_xANGMQ0C0KBfKMEIWLIu8LvAfH7W8FW2hWrwlIdYtdaTaxfA/exec'; // SUBSTITUA PELO SEU NOVO URL
+const GOOGLE_SHEET_VENDAS_URL = 'https://script.google.com/macros/s/AKfycbxA0iYdzKdZq-Sbh8q4O6bwFZfsxk_aUGTMzy9qj1pn1qdU4X5TV7wEaaT1_e_K0X53/exec'; // SUBSTITUA PELO SEU NOVO URL
 
 let sessao = JSON.parse(sessionStorage.getItem('stage_session'));
 let comparativoAtual = 'diario';
